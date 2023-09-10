0ef7c319707c4b6eeff2dc4333fcd5b51906ef5d
victorlicht <mihoubiabderrahmane09@gmail.com>
2023-09-10 10:06:25 +0100

    New Project

---
+++ src/main/java/com/victorlicht/bookstore_springboot/BookstoreSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.victorlicht.bookstore_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/victorlicht/bookstore_springboot/BookstoreSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorlicht.bookstore_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
